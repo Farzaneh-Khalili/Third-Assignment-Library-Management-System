3eec319c817034db03ad164e2c1ce84980c876c9
unknown <farzaneh.khalili.y01@gmail.com>
2023-03-06 00:27:25 +0330

    user-class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,22 @@
-public class User {
-    //User should have a list of books
-    //User should have a username and a password
+import java.util.ArrayList;
+import java.util.List;
 
-    public void rentBook(){
-        //TODO
+public class User {
+
+    private String username;
+    private String password;
+    List<String> rentedBooks = new ArrayList<>();
+    public void setUsername(String newUsername) {
+        username = newUsername;
+    }
+    public void setPassword(String newPassword) {
+        password = newPassword;
+    }
+    public void rentBook(String book){
+       rentedBooks.add(book);
     }
 
-    public void returnBook(){
-        //TODO
+    public void returnBook(String book){
+        rentedBooks.remove(book);
     }
 }
